04504a06ea69e624cf540313bfc3bf58b5ab765f
minecode0606 <minseo_0606@naver.com>
2021-02-11 00:08:38 +0900

    p.83

--- chap02/src/sec03/exam05/OperationPromotionExample.java
+++ chap02/src/sec03/exam05/OperationPromotionExample.java
@@ -15,7 +15,18 @@ public class OperationPromotionExample {
         System.out.println("유니코드: " + intvlaue2);
         System.out.println("출력문자: " + (char)intvlaue2);
 
+        int intvalue3 = 10;
+        int intvlaue4 = intvalue3/4;
+        System.out.println(intvlaue4);
 
+        int intvlaue5 = 10;
+        // int intvlaue6 = 10 / 4.0; //<- 컴파일 에러
+        double doublevalue = intvlaue5 / 4.0;
+        System.out.println(doublevalue);
 
+        int x = 1;
+        int y = 2;
+        double result = (double) x / y;
+        System.out.println(result);
     }
 }
